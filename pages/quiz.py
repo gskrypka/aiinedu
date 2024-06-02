9dc851ce425be0fa3717ef68e592c560636ad8e7
Glib Skrypka <gsk@glibs-macbook-air.home>
2024-06-02 15:48:44 +0200

    main

--- pages/quiz.py
+++ pages/quiz.py
@@ -57,7 +57,7 @@ def generate_images(prompt):
 
 # Generate quiz questions
 def generate_quiz(topic, duration, input_language, subject):
-    model = ChatOpenAI(model="gpt-4o", )
+    model = ChatOpenAI(model="gpt-4o", api_key=st.session_state.openai_api_key)
     #Open file quiz_session_prompt.txt
     with open("prompts/quiz_session_prompt.txt", "r") as file:
         system_prompt = file.read()
